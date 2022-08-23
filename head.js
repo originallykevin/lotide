cbb1156f3dc6ca63faba789a8f95ed9d783da747
originallykevin <ly1@ualberta.ca>
2022-08-23 02:01:59 +0000

    function that returns the beginning of each element in array

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    // console.log('✅✅✅ Assertion Passed: ' + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    // console.log('🛑🛑🛑 Assertion Failed: ' + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array1) {
+  return array1[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
