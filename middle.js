5aecfcbc6901be33991c77861e0b95a6488dc2e9
originallykevin <ly1@ualberta.ca>
2022-08-24 21:14:43 +0000

    function that return an array from middle of another array. either 1 element for odd array or 2 elements for even array.

---
+++ middle.js
@@ -0,0 +1,66 @@
+const eqArrays = function(array1, array2) {
+  // GUARD CLAUSE ** if array1 and array2 are of different length, return false
+  if (array1.length !== array2.length) {
+    return false
+  // else, continue rest of code
+  } else {
+  // compare if element of array1[i] === element of array2[i]
+    for (let i = 0; i < array1.length; i++) {
+      // console.log('array1[i]', array1[i])
+      // console.log('array2[i]', array2[i])
+      // looping through the elements and if one element does not equal, then it will return false and break out.
+      if (array1[i] !== array2[i]) {
+        return false;
+      } 
+    } return true
+  }
+}
+
+const assertArrayEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    // console.log('✅✅✅ Assertion Passed: ' + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    // console.log('🛑🛑🛑 Assertion Failed: ' + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// call this. Calls line 40 which calls line 11.
+assertArrayEqual([1, 2, 3], [1, 2, 3])
+
+
+// create function that passes an array
+const middle = function(array) {
+  // create variable = middleArray
+  let middleArray = []
+  // if odd return 1 element into middleArray
+  if (array.length <= 2) {
+    return []
+  } else if (array.length % 2 !== 0) {
+    middleArray.push(array[Math.floor(array.length - 1) / 2])
+  } else if (array.length % 2 === 0) {
+    middleArray.push(array[(array.length / 2) - 1])
+    middleArray.push(array[(array.length / 2)])
+  }
+  return middleArray
+}
+
+middle([1]) // => []
+// console.log('middle([1])', middle([1]))
+
+middle([1, 2]) // => []
+// console.log('middle([1, 2])', middle([1, 2]))
+
+middle([1, 2, 3]) // => [2]
+// console.log('middle([1, 2, 3])', middle([1, 2, 3]))
+
+middle([1, 2, 3, 4, 5]) // => [3]
+// console.log('middle([1, 2, 3, 4, 5])]', middle([1, 2, 3, 4, 5]))
+
+middle([1, 2, 3, 4]) // => [2, 3]
+// console.log('middle([1, 2, 3, 4])', middle([1, 2, 3, 4]))
+
+middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+// console.log('middle([1, 2, 3, 4, 5, 6])', middle([1, 2, 3, 4, 5, 6]))
+// if even return 2 elements into middleArray
