08999a3476a607e218ab7d09723c8069265e5594
originallykevin <ly1@ualberta.ca>
2022-08-24 03:30:30 +0000

    create function that checks if each element at same index matches between two arrays

---
+++ eqArrays.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    // console.log('✅✅✅ Assertion Passed: ' + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    // console.log('🛑🛑🛑 Assertion Failed: ' + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  // GUARD CLAUSE ** if array1 and array2 are of different length, return false
+  if (array1.length !== array2.length) {
+    return false
+  // else, continue rest of code
+  } else {
+  // compare if element of array1[i] === element of array2[i]
+    for (let i = 0; i < array1.length; i++) {
+      // console.log('array1[i]', array1[i])
+      // console.log('array2[i]', array2[i])
+      // looping through the elements and if one element does not equal, then it will return false and break out.
+      if (array1[i] !== array2[i]) {
+        return false;
+      } 
+    } return true
+  }
+}
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+// console.log('line 29', eqArrays([1, 2, 3], [1, 2, 3]))
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+// console.log('line 31', eqArrays([1, 2, 3], [3, 2, 1]))
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// console.log('line 33', eqArrays(["1", "2", "3"], ["1", "2", "3"]))
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+// console.log('line 35', eqArrays(["1", "2", "3"], ["1", "2", 3]))
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
