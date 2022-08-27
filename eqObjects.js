0b8278c11bc3962602a629437414afd0b62fe390
originallykevin <ly1@ualberta.ca>
2022-08-27 00:57:44 +0000

    function that check if two object are equal

---
+++ eqObjects.js
@@ -0,0 +1,55 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    // console.log('✅✅✅ Assertion Passed: ' + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    // console.log('🛑🛑🛑 Assertion Failed: ' + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const eqArrays = function(array1, array2) {
+  // GUARD CLAUSE ** if array1 and array2 are of different length, return false
+  if (array1.length !== array2.length) {
+    return false;
+    // else, continue rest of code
+  } else {
+    // compare if element of array1[i] === element of array2[i]
+    for (let i = 0; i < array1.length; i++) {
+      // console.log('array1[i]', array1[i])
+      // console.log('array2[i]', array2[i])
+      // looping through the elements and if one element does not equal, then it will return false and break out.
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    } return true;
+  }
+};
+
+const eqObjects = function(object1, object2) {
+  const array1 = Object.keys(object1);
+  const array2 = Object.keys(object2);
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (const key of Object.keys(object1)) {
+    const valOfObject1 = object1[key];
+    const valOfObject2 = object2[key];
+
+    if (Array.isArray(valOfObject1) || Array.isArray(valOfObject2)) {
+      if (!eqArrays(valOfObject1, valOfObject2))
+        return false;
+    } else if (valOfObject1 !== valOfObject2) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Returns true if both objects have identical keys with identical values.
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
