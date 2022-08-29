9fe4e8c8a9f96b6e868c6bb47b3b8628b1325c6a
originallykevin <ly1@ualberta.ca>
2022-08-29 21:01:36 +0000

    refactor with module.exports

--- middle.js
+++ middle.js
@@ -1,34 +1,5 @@
-const eqArrays = function(array1, array2) {
-  // GUARD CLAUSE ** if array1 and array2 are of different length, return false
-  if (array1.length !== array2.length) {
-    return false
-  // else, continue rest of code
-  } else {
-  // compare if element of array1[i] === element of array2[i]
-    for (let i = 0; i < array1.length; i++) {
-      // console.log('array1[i]', array1[i])
-      // console.log('array2[i]', array2[i])
-      // looping through the elements and if one element does not equal, then it will return false and break out.
-      if (array1[i] !== array2[i]) {
-        return false;
-      } 
-    } return true
-  }
-}
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    // console.log('✅✅✅ Assertion Passed: ' + actual + " === " + expected);
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    // console.log('🛑🛑🛑 Assertion Failed: ' + actual + " !== " + expected);
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// call this. Calls line 40 which calls line 11.
-assertArraysEqual([1, 2, 3], [1, 2, 3])
-
+const eqArrays = require('./eqArrays')
+const assertArraysEqual = require('./assertArraysEqual')
 
 // create function that passes an array
 const middle = function(array) {
@@ -46,27 +17,4 @@ const middle = function(array) {
   return middleArray
 }
 
-middle([1]) // => []
-// console.log('middle([1])', middle([1]))
-assertArraysEqual(middle([1]), [])
-
-middle([1, 2]) // => []
-// console.log('middle([1, 2])', middle([1, 2]))
-assertArraysEqual(middle([1, 2]), [])
-
-middle([1, 2, 3]) // => [2]
-// console.log('middle([1, 2, 3])', middle([1, 2, 3]))
-assertArraysEqual(middle([1, 2, 3]), [2])
-
-middle([1, 2, 3, 4, 5]) // => [3]
-// console.log('middle([1, 2, 3, 4, 5])]', middle([1, 2, 3, 4, 5]))
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-
-middle([1, 2, 3, 4]) // => [2, 3]
-// console.log('middle([1, 2, 3, 4])', middle([1, 2, 3, 4]))
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
-
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
-// console.log('middle([1, 2, 3, 4, 5, 6])', middle([1, 2, 3, 4, 5, 6]))
-// if even return 2 elements into middleArray
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+module.exports = middle;
