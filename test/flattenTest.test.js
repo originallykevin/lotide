a45c11c2aa4c8b93ca15226b71560602af1da06b
originallykevin <ly1@ualberta.ca>
2022-08-30 04:34:20 +0000

    refactor with mocha/chai

---
+++ test/flattenTest.test.js
@@ -0,0 +1,14 @@
+const flatten = require('../flatten');
+const assert = require('chai').assert;
+
+describe('#flatten', () => {
+  it('return [1, 2, 3, 4, 5, 6] from [1, 2, [3, 4], 5, [6]]', () => {
+    assert.deepEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+  });
+
+  it('return [1, 2, 3, 4, 5, 6] from [1, 2, [3, 4], 5, 6]', () => {
+    assert.deepEqual(flatten([1, 2, [3, 4], 5, 6]), [1, 2, 3, 4, 5, 6]);
+  });
+
+});
+
