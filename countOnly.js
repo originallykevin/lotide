abd56834706196ec325ab9aff8b3b31591964318
originallykevin <ly1@ualberta.ca>
2022-08-30 04:47:02 +0000

    refactor with module.exports

--- countOnly.js
+++ countOnly.js
@@ -1,21 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    // console.log('✅✅✅ Assertion Passed: ' + actual + " === " + expected);
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    // console.log('🛑🛑🛑 Assertion Failed: ' + actual + " !== " + expected);
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// assertEqual('Lightouse', 'Bootcamp');
-// assertEqual(1, 1);
-
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
+const assertEqual = require('./assertEqual');
 
 const countOnly = function(allItems, itemsToCount) {
-  const results = {}
+  const results = {};
   for (const item of allItems) {
     //increment only our count in result only if found in itemsToCount object
     if (itemsToCount[item]) {
@@ -23,35 +9,12 @@ const countOnly = function(allItems, itemsToCount) {
       if (results[item]) {
         results[item] += 1;
       } else {
-        results[item] = 1
+        results[item] = 1;
       }
       // console.log(item)
     }
   }
-  return results
-}
+  return results;
+};
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { 
-  "Jason": true, 
-  "Karima": true, 
-  "Fang": true, 
-  "Agouhanna": false, 
-});
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
-
+module.exports = countOnly;
